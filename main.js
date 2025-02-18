c16b99abbaf1d13491bd74f8fc7b8f3c97b690f7
IvetaBru <ivetamarkeviciute@gmail.com>
2025-02-18 13:43:59 +0200

    tab'ų sukūrimas ir  mobile first dizainas jiems

---
+++ main.js
@@ -0,0 +1,49 @@
+document
+.querySelector('#introBtn')
+.addEventListener('click', function(){
+    document.querySelector('#tab1').style.display = 'block';
+});
+
+document
+.querySelector('#closeBtn1')
+.addEventListener('click', function(){
+    document.querySelector('#tab1').style.display = 'none';
+})
+
+document
+.querySelector('#workBtn')
+.addEventListener('click', function(){
+    document.querySelector('#tab2').style.display = 'block';
+});
+
+document
+.querySelector('#closeBtn2')
+.addEventListener('click', function(){
+    document.querySelector('#tab2').style.display = 'none';
+})
+
+document
+.querySelector('#aboutBtn')
+.addEventListener('click', function(){
+    document.querySelector('#tab3').style.display = 'block';
+});
+
+document
+.querySelector('#closeBtn3')
+.addEventListener('click', function(){
+    document.querySelector('#tab3').style.display = 'none';
+})
+
+document
+.querySelector('#contactBtn')
+.addEventListener('click', function(){
+    document.querySelector('#tab4').style.display = 'block';
+});
+
+document
+.querySelector('#closeBtn4')
+.addEventListener('click', function(){
+    document.querySelector('#tab4').style.display = 'none';
+})
+
+
